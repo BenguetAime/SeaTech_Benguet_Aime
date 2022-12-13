8783cd9c718fb4a9edcf2a93ad97c6e5d24c587e
IUT GEII Toulon Table 6 <rct@univ-tln.fr>
2022-12-13 17:31:20 +0100

    c1

--- C#/RobotInterface_BENGUET/MainWindow.xaml.cs
+++ C#/RobotInterface_BENGUET/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ExtendedSerialPort;
+using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +21,55 @@ namespace RobotInterface_BENGUET
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+  
     {
+        ReliableSerialPort serialPort1;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+            serialPort1 = new ReliableSerialPort("COM4", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
         }
+
+        private void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            textBoxReception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+        }
+
+        bool toggle;
+        private void ButtonEnvoyer_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (toggle)
+            ButtonEnvoyer.Background = Brushes.RoyalBlue;
+            else ButtonEnvoyer.Background = Brushes.Beige;
+            toggle = !toggle;
+            SendMessage();
+
+
+
+        }
+
+        private void SendMessage ()
+        {
+            //textBoxReception.Text += "\n"+ "Reçu: " + textBoxEmission.Text;
+            //textBoxEmission.Text= "";
+            serialPort1.WriteLine(textBoxEmission.Text);
+            textBoxEmission.Text = "";
+
+        }
+
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+
     }
 }
