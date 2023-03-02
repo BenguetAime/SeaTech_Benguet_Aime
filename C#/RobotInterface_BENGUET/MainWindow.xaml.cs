71aff15ed6023f9a2164be1a6556639533c50678
IUT GEII Toulon Table 6 <rct@univ-tln.fr>
2023-03-02 11:02:19 +0100

    SendMessage in C#

--- C#/RobotInterface_BENGUET/MainWindow.xaml.cs
+++ C#/RobotInterface_BENGUET/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace RobotInterface_BENGUET
         public MainWindow()
         {
             InitializeComponent();
-            serialPort1 = new ReliableSerialPort("COM4", 115200, Parity.None, 8, StopBits.One);
+            serialPort1 = new ReliableSerialPort("COM15", 115200, Parity.None, 8, StopBits.One);
             serialPort1.DataReceived += SerialPort1_DataReceived;
             serialPort1.Open();
 
@@ -86,8 +86,12 @@ namespace RobotInterface_BENGUET
 
         private void SendMessage()
         {
-            serialPort1.WriteLine(textBoxEmission.Text);
-            textBoxEmission.Text = "";
+            //serialPort1.WriteLine(textBoxEmission.Text);
+            //byte[] DataSend = Encoding.ASCII.GetBytes(textBoxEmission.Text);
+            //for (int i=0;Lengt(DataSend);i++)
+
+            //serialPort1.WriteLine(DataSend);
+            //textBoxEmission.Text = "";
 
         }
 
@@ -106,14 +110,61 @@ namespace RobotInterface_BENGUET
         }
 
         byte[] byteList= new byte [20];
+        private object unsigned;
+
         private void Test_Click(object sender, RoutedEventArgs e)
         {
-           // int i;
-            for (int i= 0; i < 20; i++)
-            {
-                byteList[i] = (byte)(2*i);
-            }
-            serialPort1.Write(byteList, 0, byteList.Length);
+            // int i;
+            //for (int i= 0; i < 20; i++)
+            //{
+            //    byteList[i] = (byte)(2*i);
+            //}
+            //serialPort1.Write(byteList, 0, byteList.Length);
+            byte[] array = Encoding.ASCII.GetBytes("Bonjour");
+            UartEncodeAndSendMessage(0x0080, array.Length, array);
         }
+
+
+
+        /////////////////////////////////////////////////////////////////  TRAME
+       
+        byte CalculateChecksum(int msgFunction, int msgPayloadLength, byte[] msgPayload)
+        { 
+            byte checksum = 0;
+            checksum ^= 0xFE;
+            checksum ^= (byte)(msgFunction>>8);
+            checksum ^= (byte)(msgFunction >>0);
+            checksum ^= (byte)(msgPayloadLength>>8);
+            checksum ^= (byte)(msgPayloadLength >>0);
+            for (int i=0;i<msgPayloadLength;i++){
+                checksum ^= msgPayload[i];
+              
+            }
+            return checksum;
+
+        }
+
+        void UartEncodeAndSendMessage(int msgFunction, int msgPayloadLength, byte[] msgPayload) 
+        {
+            byte[] msg = new byte[msgPayloadLength+6];
+            msg[0] = 0xFE;
+            msg[1]=(byte)(msgFunction >> 8);
+            msg[2] =(byte)(msgFunction >> 0);
+            msg[3]= (byte)(msgPayloadLength >> 8);
+            msg[4] = (byte)(msgPayloadLength >> 0);
+            for(int i=0; i< msgPayloadLength; i++)
+            {
+                msg[5 + i] = msgPayload[i];
+            }
+            msg[5+msgPayloadLength] = CalculateChecksum(msgFunction, msgPayloadLength, msgPayload);
+
+            serialPort1.Write(msg, 0, msg.Length);
+        }
+
+
+
+
+
+
     }
 }
