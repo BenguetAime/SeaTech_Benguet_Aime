f387430218fa006e9d1b5d987921f2451e4bc102
IUT GEII Toulon Table 6 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2023-11-30 16:42:58 +0100

    Ai1

---
+++ AI/simple_peripheral_CC26X2R1_LAUNCHXL_tirtos_ccs/TacheADC/TacheADC.h
@@ -0,0 +1,17 @@
+/*
+ * TacheADC.h
+ *
+ *  Created on: 30 nov. 2023
+ *      Author: TABLE 6
+ */
+
+#ifndef TACHEADC_TACHEADC_H_
+#define TACHEADC_TACHEADC_H_
+
+void TacheADC_taskFxn(UArg, UArg);
+void TacheADC_CreateTask(void);
+void myClockSwiFxn(uintptr_t);
+
+
+
+#endif /* TACHEADC_TACHEADC_H_ */
